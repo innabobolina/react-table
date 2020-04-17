90eb39fb81a9eb1248ead7a9326a141baaa2315e
Inna <innabotchkina@Innas-MacBook-Air.local>
2020-04-17 11:09:57 -0700

    Form submit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
-import React, { Component } from 'react'
-import Table from './Table'
+
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
-  
-  state = {
-    characters: [
-      {
-        name: '1',
-        job: 'React',
-      },
-      {
-        name: '2',
-        job: 'Data Structures',
-      },
-      {
-        name: '3',
-        job: 'JavaScript',
-      },
-      {
-        name: '4',
-        job: 'Python',
-      },
-    ],
-  }
+    state = {
+        characters: []
+    };
 
-removeCharacter = index => {
-  const { characters } = this.state
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
 
-  this.setState({
-    characters: characters.filter((character, i) => {
-      return i !== index
-    }),
-  })
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render() {
+        const { characters } = this.state;
+        
+        return (
+            <div className="container">
+                <h1>React Todos Table</h1>
+                <p>Add a task with its assigned priority to the table.</p>
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
 }
 
-render() {
-  const { characters } = this.state
-
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={this.removeCharacter} />
-    </div>
-  )
-}
-}
-
-export default App
+export default App;
