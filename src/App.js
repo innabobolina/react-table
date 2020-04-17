55750b77213b9fa4c71e43afd1e1714f3f69c677
Inna <innabotchkina@Innas-MacBook-Air.local>
2020-04-17 09:11:30 -0700

    state, delete buttons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  
+  state = {
+    characters: [
       {
         name: '1',
         job: 'React',
@@ -20,14 +21,28 @@ class App extends Component {
         name: '4',
         job: 'Python',
       },
-    ]
+    ],
+  }
+
+removeCharacter = index => {
+  const { characters } = this.state
+
+  this.setState({
+    characters: characters.filter((character, i) => {
+      return i !== index
+    }),
+  })
+}
+
+render() {
+  const { characters } = this.state
 
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table characterData={characters} removeCharacter={this.removeCharacter} />
     </div>
   )
-  }
+}
 }
 
 export default App
